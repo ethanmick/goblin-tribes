36286ab99f89973d97e19ff12c36dabc0fac0d7e
Ethan Mick <ethan@ethanmick.com>
2022-09-18 15:38:05 -0400

    Add and move goblins (#1)

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,10 @@
+import { Goblin } from './goblin'
+
 export class Game extends Phaser.Scene {
   controls!: Phaser.Cameras.Controls.FixedKeyControl
+
+  units!: Phaser.Physics.Arcade.Group
+  selected?: Goblin
 
   constructor() {
     super('game')
@@ -8,9 +13,14 @@ export class Game extends Phaser.Scene {
   preload() {
     this.load.tilemapTiledJSON('map', 'assets/map_v001.json')
     this.load.image('tileset_01', 'assets/tileset_v01.png')
+    this.load.image('goblin', 'assets/goblin.png')
   }
 
   create() {
+    this.input.mouse.disableContextMenu()
+    const self = this
+    this.units = this.physics.add.group()
+
     const map = this.make.tilemap({ key: 'map' })
     const tileset = map.addTilesetImage('tileset_v01', 'tileset_01')
     const layer = map.createLayer('terrain', tileset, 0, 0)
@@ -35,6 +45,36 @@ export class Game extends Phaser.Scene {
     }
 
     this.controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig)
+
+    for (let i = 0; i < 5; i++) {
+      const goblin = new Goblin(
+        this,
+        Phaser.Math.Between(100, 200),
+        Phaser.Math.Between(100, 200)
+      )
+      this.units.add(goblin)
+
+      goblin.on(
+        'pointerup',
+        function (this: Goblin) {
+          self.selected = this
+          this.setTint(0xff0000)
+        },
+        goblin
+      )
+    }
+
+    this.input.on('pointerdown', (p: Phaser.Input.Pointer) => {
+      console.log(p)
+      if (this.selected) {
+        this.tweens.add({
+          targets: this.selected,
+          x: p.worldX,
+          y: p.worldY,
+          duration: 200,
+        })
+      }
+    })
   }
 
   update(t: number, dt: number) {
